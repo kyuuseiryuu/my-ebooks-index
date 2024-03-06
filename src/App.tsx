e20058e669accc04bb858710b1ff4204a5bbe15b
kyuuseiryuu <i@sei.ryukyu>
2024-03-06 15:38:10 +0900

    feat(pagination)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import { FormEvent, useCallback, useState } from 'react';
+import { FormEvent, useCallback, useMemo, useState } from 'react';
 import { Grid, Input, Box, List, ListItem, ListItemContent, Typography, ListItemButton, IconButton } from '@mui/joy';
 import Download from '@mui/icons-material/Download';
-import { groupBy } from 'lodash';
+import lodash, { groupBy } from 'lodash';
 import './App.css';
 import { TreeNode } from './types';
 import FolderTree from './components/FolderTree';
 import rawData from './assets/result.json';
+import { Pagination } from 'antd';
 
 function getTreeData(data: typeof rawData) {
   const treeRoot: TreeNode[] = [];
@@ -39,12 +40,17 @@ const treeData = getTreeData(rawData);
 function App() {
   const [searchKey, setSearchKey] = useState('');
   const [showList, setShowList] = useState<typeof rawData>([]);
+  const [pagination, setPagination] = useState({ pageSize: 20, currentPage: 1 });
 
   const handleSubmit = useCallback((e: FormEvent) => {
     e.preventDefault();
     console.debug('submit', searchKey);
     const list = searchKey ? rawData.filter(e => e.path.includes(searchKey) || e.name.includes(searchKey)) : [];
     setShowList([...list]);
+    setPagination(info => ({
+      ...info,
+      currentPage: 1,
+    }));
   }, [searchKey]);
 
 
@@ -55,6 +61,7 @@ function App() {
       console.debug(list);
       setShowList(list);
       setSearchKey('');
+      setPagination(info => ({ ...info, currentPage: 1 }));
     }
   }, []);
 
@@ -76,26 +83,43 @@ function App() {
         <Box maxHeight='800px' position='sticky' mt={1} overflow='auto'>
           <FolderTree treeData={treeData} onClick={e => handleClick(e)} />
         </Box>
+        <Box mt={1}>
+          <Pagination
+            size='small'
+            showSizeChanger={false}
+            pageSize={pagination.pageSize}
+            current={pagination.currentPage}
+            total={showList.length}
+            onChange={(page, size) => {
+              setPagination({
+                pageSize: size,
+                currentPage: page,
+              });
+            }}
+          />
+        </Box>
       </Grid>
       <Grid m={1} xs={10} sm={6} md={6} xl={8} height='100%' display='flex' flexDirection='row' overflow='auto' flexWrap='wrap'>
         <List>
-          {showList.map((e, i) => (
-            <ListItem
-              key={`${e.name}-${i}`}
-              startAction={
-                <IconButton onClick={() => handleDownload(e)}>
-                  <Download />
-                </IconButton>
-              }
-            >
-              <ListItemButton>
-                <ListItemContent>
-                  <Typography level='title-lg'>{e.name}</Typography>
-                  <Typography level='body-sm'>{e.path}</Typography>
-                </ListItemContent>
-              </ListItemButton>
-            </ListItem>
-          ))}
+          {showList
+            .slice(pagination.pageSize * (pagination.currentPage - 1), pagination.pageSize * pagination.currentPage)
+            .map((e, i) => (
+              <ListItem
+                key={`${e.name}-${i}`}
+                startAction={
+                  <IconButton onClick={() => handleDownload(e)}>
+                    <Download />
+                  </IconButton>
+                }
+              >
+                <ListItemButton>
+                  <ListItemContent>
+                    <Typography level='title-lg'>{e.name}</Typography>
+                    <Typography level='body-sm'>{e.path}</Typography>
+                  </ListItemContent>
+                </ListItemButton>
+              </ListItem>
+            ))}
         </List>
       </Grid>
     </Grid>
